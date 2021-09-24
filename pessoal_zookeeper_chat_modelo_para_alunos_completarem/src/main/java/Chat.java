155d875beb425c13ac4a0bb0170132ae25452ea3
JayJatoba <jguilhermejatoba@hotmail.com>
2021-09-23 21:56:43 -0300

    Implementar classe Scanner

--- pessoal_zookeeper_chat_modelo_para_alunos_completarem/src/main/java/Chat.java
+++ pessoal_zookeeper_chat_modelo_para_alunos_completarem/src/main/java/Chat.java
@@ -22,7 +22,7 @@ public class Chat {
     //para abrigar todos os usuários atualmente logados no chat
     private static final String ZNODE_USUARIOS = "/usuarios";
     //para ler os dados que o usuário vai digitar
-    private Scanner scanner;
+    private Scanner scanner = new Scanner(System.in);
     //menu de opções a ser exibido para o usuário, uma vez que ele tenha logado no chat
     private String instrucoes =
             String.format(
